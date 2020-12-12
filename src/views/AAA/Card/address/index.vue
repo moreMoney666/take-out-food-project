c825201ac1c4e51b6c06af8d1f85eb5b4185c266
jingwen <jingwen@qq.com>
2020-12-12 16:05:54 +0800

    'addressOver'

--- src/views/AAA/Card/address/index.vue
+++ src/views/AAA/Card/address/index.vue
@@ -47,11 +47,13 @@
       </el-form-item>
       <el-form-item>
         <div v-if="$route.query.isEdit">
-          <el-button type="primary">保存</el-button>
-          <el-button>取消</el-button>
+          <el-button type="primary" @click="submitForm('ruleForm')">保存</el-button>
+          <el-button @click="$router.replace('/card/cartList')">取消</el-button>
         </div>
         <div v-else>
-          <el-button type="primary" @click="submitForm">立即创建</el-button>
+          <el-button type="primary" @click="submitForm('ruleForm')"
+            >立即创建</el-button
+          >
           <el-button @click="resetForm()">重置</el-button>
         </div>
       </el-form-item>
@@ -62,19 +64,28 @@
 import { v4 as uuidv4 } from "uuid";
 export default {
   name: "Address",
+  mounted() {
+    if(this.$route.query.isEdit){
+      const attr = JSON.parse(localStorage.getItem("addressList"));
+      const index = this.$route.query.i * 1;
+      this.ruleForm = attr[index];
+    }
+
+  },
   data() {
     return {
       //  修改拿回来的数据要存储得地方
       addressList: [],
       ruleForm: {
+        phone: "", //收货电话
         title: "", //收货地址
         name: "", //收货姓名
-        phone: "", //收货电话
         city: "", //所在城市
         detail: "", //详细地址
         type: [], //地址类型
         gender: "", //性别
-        desc: "" //备注
+        desc: "", //备注
+        index: 0
       },
       // 表单验证
       rules: {
@@ -115,23 +126,52 @@ export default {
     };
   },
   methods: {
+    clearForm() {
+      this.ruleForm = {
+        title: "", //收货地址
+        name: "", //收货姓名
+        phone: "", //收货电话
+        city: "", //所在城市
+        detail: "", //详细地址
+        type: [], //地址类型
+        gender: "", //性别
+        desc: "", //备注
+        index: 0
+      };
+    },
     // 点击立即创建按钮,jiang
-    submitForm() {
-      let addressList;
-      if (localStorage.getItem("addressList")) {
-        addressList = JSON.parse(localStorage.getItem("addressList"));
-      }
-      this.ruleForm.id = uuidv4();
-      // console.log(this.addressList);
-      addressList.push(this.ruleForm);
-      this.addressList = addressList;
-      // console.log(this.addressList);
-      localStorage.setItem("addressList", JSON.stringify(this.addressList));
-      this.$message.success("添加成功");
-      this.$router.push("/card/cartList");
+    submitForm(forName) {
+      console.log(forName);
+      this.$refs[forName].validate(valid => {
+        if (valid) {
+            let addressList = [];
+            if (localStorage.getItem("addressList")) {
+              addressList = JSON.parse(localStorage.getItem("addressList"));
+              this.addressList = addressList
+            }
+            // 修改
+            if(this.$route.query.isEdit){
+              let index = this.$route.query.i
+              addressList.splice(index,1,this.ruleForm)
+
+            }else{
+              // 新增
+              this.ruleForm.id = uuidv4();
+              // console.log(this.addressList);
+              addressList.push(this.ruleForm);
+              this.addressList = addressList;
+              // console.log(this.addressList); 
+            }
+            localStorage.setItem("addressList", JSON.stringify(this.addressList));
+            this.$message.success("操作成功");
+            this.$router.push("/card/cartList");  
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
     },
     resetForm() {
-      // this.$refs[formName].resetFields();
       this.ruleForm = {
         title: "", //收货地址
         name: "", //收货姓名
